5f330b2af04736e235570d0c5c7b46d61135f53f
Gary Green <holegary@gmail.com>
2018-07-30 21:32:42 +0100

    Dist.

--- dist/disabler.js
+++ dist/disabler.js
@@ -1,147 +1,143 @@
-/*
- *  Copyright 2015 Gary Green.
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
+var Disabler = (function () {
+	'use strict';
 
-function Disabler(element, options) {
-	this.init(element, options);
-}
-
-Disabler.prototype = {
-
-	defaults: {
-		timeout: 20000,
-		html: undefined,
-		auto: false
-	},
-
-	/**
-	 * Initialise disabler on element with the given options.
+	/*
+	 *  Copyright 2018 Gary Green.
+	 *  Licensed under the Apache License, Version 2.0 (the "License");
+	 *  you may not use this file except in compliance with the License.
+	 *  You may obtain a copy of the License at
 	 *
-	 * @param  {mixed} element
-	 * @param  {object} options
+	 *  http://www.apache.org/licenses/LICENSE-2.0
 	 *
-	 * @return {void}
+	 *  Unless required by applicable law or agreed to in writing, software
+	 *  distributed under the License is distributed on an "AS IS" BASIS,
+	 *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	 *  See the License for the specific language governing permissions and
+	 *  limitations under the License.
 	 */
-	init: function(element, options) {
-		this.$element = $(element);
-		this.setOptions(options);
 
-		if (this.options.auto) {
-			this.debounce();
-		} else {
-			this.bindEvents();
-		}
-	},
-
-	/**
-	 * Set the options.
-	 *
-	 * @param {object} options
-	 */
-	setOptions: function(options) {
-		this.options = $.extend({}, this.defaults, options);
-
-		var loadingMessage = this.$element.data('disabler');
-		if (options.html === undefined && typeof loadingMessage === 'string' && loadingMessage.length) {
-			this.options.html = loadingMessage;
-		}
-	},
-
-	/**
-	 * Bind events which trigger disabling the element.
-	 *
-	 * @return {void}
-	 */
-	bindEvents: function() {
-		this.$element.on('click', $.proxy(this.debounce, this));
-	},
-
-	/**
-	 * Debounce disabling/enabling the element.
-	 *
-	 * @return {void}
-	 */
-	debounce: function() {
-		setTimeout($.proxy(function() {
-			this.disable();
-		}, this), 0);
-
-		this.timer = setTimeout($.proxy(function() {
-			this.enable();
-		}, this), this.options.timeout);
-	},
-
-	/**
-	 * Get html to add.
-	 *
-	 * @return {string}
-	 */
-	getHtml: function() {
-		var html = this.options.html;
-		if ($.isFunction(html)) {
-			html = html.call(this, this.$element);
-		}
-
-		return html;
-	},
-
-	/**
-	 * Disable the element.
-	 *
-	 * @return {void}
-	 */
-	disable: function() {
-		this.$element.prop('disabled', true);
-
-		var html = this.getHtml();
-		if (html === undefined) return;
-
-		this.oldHtml = this.$element.html();
-		this.$element.html(html);
-	},
-
-	/**
-	 * Enable the element.
-	 *
-	 * @return {void}
-	 */
-	enable: function() {
-		if (this.timer) {
-			clearTimeout(this.timer);
-		}
-
-		this.$element.prop('disabled', false).html(this.oldHtml);
-		this.oldHtml = '';
+	function Disabler(element, options) {
+		this.init(element, options);
 	}
 
-};
+	Disabler.prototype = {
 
-$.fn.disabler = function(options) {
+		defaults: {
+			timeout: 20000,
+			html: undefined,
+			auto: false
+		},
 
-	options = options || {};
+		/**
+		 * Initialise disabler on element with the given options.
+		 *
+		 * @param  {mixed} element
+		 * @param  {object} options
+		 *
+		 * @return {void}
+		 */
+		init: function(element, options) {
+			this.element = element;
+			this.setOptions(options);
 
-	var namespace = 'ggdisabler';
+			if (this.options.auto) {
+				this.debounce();
+			} else {
+				this.bindEvents();
+			}
+		},
 
-	return $(this).each(function() {
-		var $this = $(this);
+		/**
+		 * Set the options.
+		 *
+		 * @param {object} options
+		 */
+		setOptions: function(options) {
+			this.options = this._simpleMerge(this.defaults, options);
+			
+			var loadingMessage = this.element.getAttribute('data-disabler');
+			if (options.html === undefined && typeof loadingMessage === 'string' && loadingMessage.length) {
+				this.options.html = loadingMessage;
+			}
+		},
 
-		var disabler = $this.data(namespace);
-		if (disabler) {
-			disabler.init(this, options);
-		} else {
-			$this.data(namespace, new Disabler(this, options));
+		/**
+		 * Bind events which trigger disabling the element.
+		 *
+		 * @return {void}
+		 */
+		bindEvents: function() {
+			this.element.addEventListener('click', this.debounce.bind(this));
+		},
+
+		/**
+		 * Debounce disabling/enabling the element.
+		 *
+		 * @return {void}
+		 */
+		debounce: function() {
+			setTimeout(this.disable.bind(this), 0);
+			this.timer = setTimeout(this.enable.bind(this), this.options.timeout);
+		},
+
+		/**
+		 * Get html to add.
+		 *
+		 * @return {string}
+		 */
+		getHtml: function() {
+			var html = this.options.html;
+			if (typeof html === 'function') {
+				html = html.call(this, this.element);
+			}
+
+			return html;
+		},
+
+		/**
+		 * Disable the element.
+		 *
+		 * @return {void}
+		 */
+		disable: function() {
+			this.element.disabled = true;
+
+			var html = this.getHtml();
+			if (html === undefined) return;
+
+			this.oldHtml = this.element.innerHTML;
+			this.element.innerHTML = html;
+		},
+
+		/**
+		 * Enable the element.
+		 *
+		 * @return {void}
+		 */
+		enable: function() {
+			if (this.timer) {
+				clearTimeout(this.timer);
+			}
+
+			this.element.disabled = false;
+			this.element.innerHTML = this.oldHtml;
+			this.oldHtml = '';
+		},
+
+		_simpleMerge: function(obj1, obj2) {
+			// It be nice to use Object.assign to avoid haing to write this function
+			// but unfortunately IE11 doesn't support it.
+			var ret = Object.create(obj1), prop;
+
+			for (prop in obj2) {
+				ret[prop] = obj2[prop];
+			}
+
+			return ret;
 		}
-	});
 
-};
+	};
+
+	return Disabler;
+
+}());
